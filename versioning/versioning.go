a0d7175e472c0821f674ca9502f0aedf50137cd4
Your Name <you@example.com>
2023-11-30 14:07:44 +0600

    update versioning

--- versioning/versioning.go
+++ versioning/versioning.go
@@ -7,8 +7,8 @@ var (
 	// Embedded by --ldflags on build time
 	// Versioning should follow the SemVer guidelines
 	// https://semver.org/
-	Version   string
-	Branch    string
+	Version   = "1.0.3_beta"
+	Branch    = "dev"
+	BuildTime = "1/12/2023"
 	Commit    string
-	BuildTime string
 )
