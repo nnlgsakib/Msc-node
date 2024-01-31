9ebe9b9392fe9736fff88197487a5c92c1e25761
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-01-31 16:38:09 +0600

    Update eth_endpoint.go

--- jsonrpc/eth_endpoint.go
+++ jsonrpc/eth_endpoint.go
@@ -401,7 +401,7 @@ func (e *Eth) GetStorageAt(
 // }
 func (e *Eth) GasPrice() (interface{}, error) {
 	// Return constant gas price of 10 Gwei (10^9 Wei)
-	const fixedGasPrice = 10^10
+	const fixedGasPrice = 31693127296
 	return fixedGasPrice, nil
 }
 
