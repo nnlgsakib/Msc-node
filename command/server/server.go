47ba6a5f3c6d2f873b422e444a73b6ac73525712
DTX-DEV <nlgarts@outlook.com>
2024-09-08 09:36:19 +0600

    removed unused file

--- command/server/server.go
+++ command/server/server.go
@@ -214,12 +214,12 @@ func setFlags(cmd *cobra.Command) {
 		"write all logs to the file at specified location instead of writing them to console",
 	)
 
-	cmd.Flags().BoolVar(
-		&params.rawConfig.Relayer,
-		relayerFlag,
-		defaultConfig.Relayer,
-		"start the state sync relayer service (PolyBFT only)",
-	)
+	// cmd.Flags().BoolVar(
+	// 	&params.rawConfig.Relayer,
+	// 	relayerFlag,
+	// 	defaultConfig.Relayer,
+	// 	"start the state sync relayer service (PolyBFT only)",
+	// )
 
 	cmd.Flags().Uint64Var(
 		&params.rawConfig.NumBlockConfirmations,
